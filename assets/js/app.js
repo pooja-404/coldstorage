a69cf1416e6e59b400a3fe6c642a8c1f7d13ddf8
pooja-404 <pk3924792@gmail.com>
2024-05-30 20:37:19 +0530

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,36 @@ window.addEventListener('scroll', function () {
 })
 
 
+// script.js
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    
+    // Ensure the index is within the bounds
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    // Move the slides container to show the current slide
+    const slidesContainer = document.querySelector('.slides');
+    slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Optionally, add automatic sliding
+setInterval(() => {
+    nextSlide();
+}, 3000);
